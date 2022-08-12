72e2ab07d20d2cc9373e9385a012aa676ec46976
DY-Shin <109335554+DY-Shin@users.noreply.github.com>
2022-08-12 16:36:21 +0900

    [D3] Title: 소득 불균형, Time: 246 ms, Memory: 63,048 KB -BaekjoonHub

---
+++ SWEA/D3/10505. 소득 불균형/소득 불균형.py
@@ -0,0 +1,17 @@
+T = int(input())
+for test_case in range(1, T + 1):
+    N = int(input())
+    arr = list(map(int, input().split()))
+    total = 0
+    cnt = 0
+
+    for i in range(N):
+        total += arr[i]
+
+    average = total // N
+
+    for i in range(N):
+        if arr[i] <= average:
+            cnt += 1
+
+    print(f'#{test_case} {cnt}')
