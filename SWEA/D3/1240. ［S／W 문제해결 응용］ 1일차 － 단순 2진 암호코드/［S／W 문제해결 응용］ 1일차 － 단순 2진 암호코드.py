1481b71605d08a61a588f21242778736e0651a04
DY-Shin <109335554+DY-Shin@users.noreply.github.com>
2022-09-19 17:33:47 +0900

    [D3] Title: [S/W 문제해결 응용] 1일차 - 단순 2진 암호코드, Time: 152 ms, Memory: 59,312 KB -BaekjoonHub

--- SWEA/D3/1240. ［S／W 문제해결 응용］ 1일차 － 단순 2진 암호코드/［S／W 문제해결 응용］ 1일차 － 단순 2진 암호코드.py
+++ SWEA/D3/1240. ［S／W 문제해결 응용］ 1일차 － 단순 2진 암호코드/［S／W 문제해결 응용］ 1일차 － 단순 2진 암호코드.py
@@ -1,32 +1,34 @@
 T = int(input())
 for test_case in range(1, T + 1):
     N, M = map(int, input().split())
-    arr = [list(map(str, input())) for _ in range(N)]
     bin_pass = ['0001101', '0011001', '0010011', '0111101', '0100011',
                 '0110001', '0101111', '0111011', '0110111', '0001011']
-    result = []
     num = []
-    k = 0
-    l = 0
-    for i in range(N):
-        for j in range(M - 1, -1, -1):
-            if arr[i][j] == '1':
-                password = arr[i][j - 55:j + 1]
-                break
-
+    odd = 0
+    even = 0
+    flag = 1
+    for _ in range(N):
+        arr = input()
+        if flag:
+            for i in range(M - 1, 55, -1):
+                if arr[i] == '1':
+                    password = arr[i - 55:i + 1]
+                    flag = 0
+                    break
+ 
     for i in range(0, 56, 7):
-        result.append(''.join(password[i:i + 7]))
-    for i in range(8):
+        result = password[i:i + 7]
         for j in range(10):
-            if result[i] == bin_pass[j]:
+            if result == bin_pass[j]:
                 num.append(j)
-    for i in range(0, 8, 2):
-        k += num[i]
-    for i in range(1, 8, 2):
-        l += num[i]
-    test = k * 3 + l
+    for i in range(8):
+        if i % 2:
+            even += num[i]
+        else:
+            odd += num[i]
+    test = odd * 3 + even
     print(f'#{test_case}', end=' ')
     if test % 10:
         print(0)
     else:
-        print(k + l)
+        print(odd + even)
