78d3b16abe68d9c1efc959a6b050aabf6dac7b81
DY-Shin <109335554+DY-Shin@users.noreply.github.com>
2022-09-23 14:26:02 +0900

    [D4] Title: 정식이의 은행업무, Time: 185 ms, Memory: 62,912 KB -BaekjoonHub

--- SWEA/D4/4366. 정식이의 은행업무/정식이의 은행업무.py
+++ SWEA/D4/4366. 정식이의 은행업무/정식이의 은행업무.py
@@ -1,37 +1,33 @@
 import copy
 
+
+def func(arr, n):
+    table = []
+    result = []
+    ans = []
+    for i in range(n):
+        table.append(str(i))
+    for i in range(len(arr)):
+        for j in range(n):
+            if arr[i] != table[j]:
+                new_arr = copy.deepcopy(arr)
+                new_arr[i] = table[j]
+                result.append(''.join(new_arr))
+
+    for i in range(len(result)):
+        calc = 0
+        for j in range(len(result[i])):
+            calc += int(result[i][j]) * (n**(len(result[i]) - j - 1))
+        ans.append(calc)
+    return ans
+
+
 T = int(input())
 for test_case in range(1, T + 1):
     bin_num = list(map(str, input()))
     tri_num = list(map(str, input()))
-    bin_table = ['0', '1']
-    tri_table = ['0', '1', '2']
-    bin_result = []
-    tri_result = []
-    bin_ans = []
-    tri_ans = []
-    for i in range(len(bin_num)):
-        for j in range(2):
-            if bin_num[i] != bin_table[j]:
-                new_bin = copy.deepcopy(bin_num)
-                new_bin[i] = bin_table[j]
-                bin_result.append(''.join(new_bin))
-
-    for i in range(len(tri_num)):
-        for j in range(3):
-            if tri_num[i] != tri_table[j]:
-                new_tri = copy.deepcopy(tri_num)
-                new_tri[i] = tri_table[j]
-                tri_result.append(''.join(new_tri))
-
-    for i in range(len(tri_result)):
-        calc = 0
-        for j in range(len(tri_result[i])):
-            calc += int(tri_result[i][j]) * (3**(len(tri_result[i]) - j - 1))
-        tri_ans.append(calc)
-    for i in range(len(bin_result)):
-        bin_ans.append(int('0b' + bin_result[i], 2))
-
+    bin_ans = func(bin_num, 2)
+    tri_ans = func(tri_num, 3)
     for i in bin_ans:
         if i in tri_ans:
             print(f'#{test_case} {i}')
