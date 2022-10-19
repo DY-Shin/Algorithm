24c6a4c588f8ff9e02d1934cd2c324c9e0423d9b
DY-Shin <109335554+DY-Shin@users.noreply.github.com>
2022-10-19 15:45:04 +0900

    [Silver III] Title: N과 M (2), Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.py
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -1,13 +1,7 @@
-def comb(n, r, k, s):
-    if k == r:
-        print(*t)
-    else:
-        for i in range(s, n - r + 1 + k):
-            t[k] = arr[i]
-            comb(n, r, k + 1, i + 1)
-
+from itertools import combinations
 
 N, M = map(int, input().split())
 arr = [i for i in range(1, N + 1)]
-t = [0] * M
-comb(N, M, 0, 0)
+combination = combinations(arr, M)
+for v in combination:
+    print(*v)
