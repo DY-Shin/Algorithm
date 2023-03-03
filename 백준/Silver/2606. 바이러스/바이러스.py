86679a1968f9d1f79cf35160533bb0b6da9f5b24
DY-Shin <109335554+DY-Shin@users.noreply.github.com>
2023-03-03 13:18:02 +0900

    [Silver III] Title: 바이러스, Time: 72 ms, Memory: 34140 KB -BaekjoonHub

---
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -0,0 +1,25 @@
+from collections import deque
+
+
+def bfs(v):
+    q = deque()
+    q.append(v)
+    visited[v] = 1
+    while q:
+        n = q.popleft()
+        for w in adj_list[n]:
+            if visited[w] == 0:
+                q.append(w)
+                visited[w] = 1
+
+
+N = int(input())
+E = int(input())
+adj_list = [[] for _ in range(N + 1)]
+visited = [0] * (N + 1)
+for i in range(E):
+    s, e = map(int, input().split())
+    adj_list[s].append(e)
+    adj_list[e].append(s)
+bfs(1)
+print(sum(visited) - 1)
