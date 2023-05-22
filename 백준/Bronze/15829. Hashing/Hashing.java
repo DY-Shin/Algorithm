98a6e1da404deddd403321da2c4fdafb7429dc72
신동윤 <109335554+DY-Shin@users.noreply.github.com>
2023-05-22 16:51:58 +0900

    [Bronze II] Title: Hashing, Time: 124 ms, Memory: 14232 KB -BaekjoonHub

---
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int L = Integer.parseInt(br.readLine());
+        int result = 0;
+        String line = br.readLine();
+        HashMap<String, Integer> map = new HashMap<>();
+        for (int i = 1; i < 27; i++) {
+            map.put(String.valueOf((char)(96 + i)), i);
+        }
+        for (int i = 0; i < L; i++) {
+            int tmp = map.get(String.valueOf(line.charAt(i)));
+            result += tmp * Math.pow(31, i);
+        }
+        System.out.println(result);
+    }
+}
