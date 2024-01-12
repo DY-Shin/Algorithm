e482d662ae8ab1b6dd0db31984af8e8e1f2f147b
신동윤 <109335554+DY-Shin@users.noreply.github.com>
2024-01-13 05:58:57 +0900

    [Silver IV] Title: 대피소, Time: 1936 ms, Memory: 31120 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/28215. 대피소/대피소.py
+++ 백준/Silver/28215. 대피소/대피소.py
@@ -3,26 +3,19 @@ import sys
 input = sys.stdin.readline
 
 N, K = map(int, input().split())
-house = [list(map(int, input().split())) for _ in range(N)]
-entire = list(range(N))
-com = list(combinations(entire, K))
+x = [0] * N
+y = [0] * N
 answer = 987654321
-for i in range(len(com)):
-    shelter = []
-    normal = []
+for i in range(N):
+    x[i], y[i] = map(int, input().split())
+
+for c in combinations(range(N), K):
+    case = 0
     for j in range(N):
-        if j in com[i]:
-            shelter.append(house[j])
-        else:
-            normal.append(house[j])
-
-    dis_list = [987654321] * (N - K)
-    for j in range(K):
-        for k in range(N - K):
-            dis = abs(shelter[j][0] - normal[k][0]) + abs(shelter[j][1] - normal[k][1])
-            if dis_list[k] > dis:
-                dis_list[k] = dis
-        if max(dis_list) < answer:
-            answer = max(dis_list)
-
+        dis = 987654321
+        for k in c:
+            tmp = abs(x[j] - x[k]) + abs(y[j] - y[k])
+            dis = min(tmp, dis)
+        case = max(case, dis)
+    answer = min(answer, case)
 print(answer)
